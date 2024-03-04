49202d0239828d19761c8b6bf1866b5e0b3e73e7
krushna-shinde <krishnashinde1996@gmail.com>
2024-03-04 22:39:23 +0530

    code updated

--- src/Pages/DetailsFilling.js
+++ src/Pages/DetailsFilling.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const DetailsFilling = () => {
   return (
     <div>
       <h1>Details_Filling</h1>
     </div>
-  )
-}
+  );
+};
 
-export default DetailsFilling
+export default DetailsFilling;
