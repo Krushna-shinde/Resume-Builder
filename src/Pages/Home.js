b700c1e890ef2a6ff27bdbcfc54455af00a459e0
krushna-shinde <krishnashinde1996@gmail.com>
2024-03-11 10:18:11 +0530

    code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
       <h1>home</h1>
+      <h3>details</h3>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
